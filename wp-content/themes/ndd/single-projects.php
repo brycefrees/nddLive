6461da2dab44f5edb804c66842b3897079ec5f79
brycefrees <bryce@brycefrees.com>
2015-08-20 10:34:46 -0700

    got rid of lightbox link around slider image

--- wp-content/themes/ndd/single-projects.php
+++ wp-content/themes/ndd/single-projects.php
@@ -71,9 +71,12 @@
 									      
 									    <?php foreach( $images as $image ): ?>
 								        <div class="item">
-								          <a href="<?php echo $image['url']; ?>" rel="lightbox"><img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" /></a>
-										  <a href="<?php echo $image['url']; ?>" rel="lightbox" class="fullscreenBTN"></a>
-								        </div>
+								         <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
+								          <a href="<?php echo $image['url']; ?>" rel="lightbox" class="fullscreenBTN"></a>
+
+										 </div>
+										 
+										 
 								        
 								         <?php endforeach; ?>
 								       <?php endif; ?>
